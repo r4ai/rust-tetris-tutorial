486f42d4c4e8f6d118adc287ae12cf3925c27b2e
rai <mj82xx@gmail.com>
2023-02-12 21:10:44 +0900

    全てのブロックデータを描画してみる

--- src/main.rs
+++ src/main.rs
@@ -63,12 +63,6 @@ const BLOCKS: [BlockShape; 7] = [
 ];
 
 fn main() {
-    let block = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-    ];
     let field = [
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -97,9 +91,13 @@ fn main() {
     let mut field_buf = field;
     for y in 0..4 {
         for x in 0..4 {
-            if block[y][x] == 1 {
-                field_buf[y + 8][x + 4] = 1;
-            }
+            field_buf[y + 2][x + 2] = BLOCKS[BlockKind::I as usize][y][x];
+            field_buf[y + 2][x + 7] = BLOCKS[BlockKind::O as usize][y][x];
+            field_buf[y + 6][x + 2] = BLOCKS[BlockKind::S as usize][y][x];
+            field_buf[y + 6][x + 7] = BLOCKS[BlockKind::Z as usize][y][x];
+            field_buf[y + 10][x + 2] = BLOCKS[BlockKind::J as usize][y][x];
+            field_buf[y + 10][x + 7] = BLOCKS[BlockKind::L as usize][y][x];
+            field_buf[y + 14][x + 2] = BLOCKS[BlockKind::T as usize][y][x];
         }
     }
 
