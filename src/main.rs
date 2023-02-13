fa6df298a8f07408ce69a960dcbd8c47f2605f05
rai <mj82xx@gmail.com>
2023-02-13 11:05:06 +0900

    当たり判定の実装

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::{thread::sleep, time::Duration};
 
+const FIELD_WIDTH: usize = 11 + 2;
+const FIELD_HEIGHT: usize = 20 + 1;
+type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
+
+#[derive(Debug, Clone, Copy)]
 enum BlockKind {
     I,
     O,
@@ -11,7 +16,6 @@ enum BlockKind {
 }
 
 type BlockShape = [[usize; 4]; 4];
-
 const BLOCKS: [BlockShape; 7] = [
     // I
     [
@@ -69,6 +73,17 @@ struct Position {
     y: usize,
 }
 
+fn is_collision(field: &Field, pos: &Position, block: BlockKind) -> bool {
+    for y in 0..4 {
+        for x in 0..4 {
+            if field[y + pos.y + 1][x + pos.x] & BLOCKS[block as usize][y][x] == 1 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
 fn main() {
     let field = [
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -100,7 +115,7 @@ fn main() {
     println!("\x1b[2J\x1b[H\x1b[?25l");
 
     // 5マス分落下させる
-    for _ in 0..5 {
+    for _ in 0..30 {
         // 裏データの更新
         let mut field_buf = field;
         for y in 0..4 {
@@ -112,12 +127,14 @@ fn main() {
         }
 
         // 座標を更新
-        pos.y += 1;
+        if !is_collision(&field, &pos, BlockKind::I) {
+            pos.y += 1;
+        }
 
         // 描画
         println!("\x1b[H"); // カーソルを左上に移動
-        for y in 0..21 {
-            for x in 0..13 {
+        for y in 0..FIELD_HEIGHT {
+            for x in 0..FIELD_WIDTH {
                 if field_buf[y][x] == 1 {
                     print!("[]");
                 } else {
