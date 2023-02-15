d9dc1fd4bb7d83bc49ed65d66512087336f39ade
rai <mj82xx@gmail.com>
2023-02-15 16:22:47 +0900

    ゲームモード選択機能の実装

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,30 @@
 mod block;
 mod game;
+mod play;
 
-use game::{Game, Position};
-use getch_rs::{Getch, Key};
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::{thread::sleep, time::Duration};
+use clap::{Parser, Subcommand};
 
-use crate::game::{
-    draw, gameover, hard_drop, hold, is_collision, landing, move_block, quit, rotate_left,
-    rotate_right,
-};
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    mode: Option<Mode>,
+}
+
+#[derive(Subcommand)]
+enum Mode {
+    Normal,
+    Auto,
+}
 
 fn main() {
-    // ゲームの初期化
-    let game = Arc::new(Mutex::new(Game::new()));
+    let cli = Cli::parse();
 
-    // 画面クリア
-    println!("\x1b[2J\x1b[H\x1b[?25l");
-
-    // 初回描画(フィールドの描画)
-    draw(&game.lock().unwrap());
-
-    // 自然落下処理
-    {
-        let game = Arc::clone(&game);
-
-        thread::spawn(move || {
-            loop {
-                // 10ライン消すごとに、100ミリ秒速くすることにする
-                let sleep_msec =
-                    match 1000u64.saturating_sub((game.lock().unwrap().line as u64 / 10) * 100) {
-                        0 => 100,
-                        msec => msec,
-                    };
-                sleep(Duration::from_millis(sleep_msec));
-
-                // 必要な変数の取得
-                let mut game = game.lock().unwrap();
-
-                // 自然落下
-                let new_pos = Position {
-                    x: game.pos.x,
-                    y: game.pos.y + 1,
-                };
-                if !is_collision(&game.field, &new_pos, &game.block) {
-                    // ブロックの移動
-                    game.pos = new_pos;
-                } else if landing(&mut game).is_err() {
-                    gameover(&game);
-                }
-
-                // 裏データの描画
-                draw(&game);
-            }
-        });
-    }
-
-    // キー入力処理
-    let g = Getch::new();
-    loop {
-        match g.getch() {
-            Ok(Key::Left) => {
-                let mut game = game.lock().unwrap();
-                let new_pos = Position {
-                    x: game.pos.x.checked_sub(1).unwrap_or(game.pos.x),
-                    y: game.pos.y,
-                };
-                move_block(&mut game, new_pos);
-                draw(&game);
-            }
-            Ok(Key::Right) => {
-                let mut game = game.lock().unwrap();
-                let new_pos = Position {
-                    x: game.pos.x + 1,
-                    y: game.pos.y,
-                };
-                move_block(&mut game, new_pos);
-                draw(&game);
-            }
-            Ok(Key::Down) => {
-                let mut game = game.lock().unwrap();
-                let new_pos = Position {
-                    x: game.pos.x,
-                    y: game.pos.y + 1,
-                };
-                move_block(&mut game, new_pos);
-                draw(&game);
-            }
-            Ok(Key::Up) => {
-                let mut game = game.lock().unwrap();
-                hard_drop(&mut game);
-                if landing(&mut game).is_err() {
-                    gameover(&game);
-                }
-                draw(&game);
-            }
-            Ok(Key::Char('x')) => {
-                let mut game = game.lock().unwrap();
-                rotate_right(&mut game);
-                draw(&game);
-            }
-            Ok(Key::Char('z')) => {
-                let mut game = game.lock().unwrap();
-                rotate_left(&mut game);
-                draw(&game);
-            }
-            Ok(Key::Char(' ')) => {
-                let mut game = game.lock().unwrap();
-                hold(&mut game);
-                draw(&game);
-            }
-            Ok(Key::Char('q')) => quit(),
-            _ => {}
+    match cli.mode {
+        None | Some(Mode::Normal) => {
+            play::normal();
+        }
+        Some(Mode::Auto) => {
+            play::auto();
         }
     }
 }
